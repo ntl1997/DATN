c1cbe853a5149be3956418126d7d71543afa455a
HieuNT-2001 <hieuth0109@gmail.com>
2025-06-09 23:48:41 +0700

    display user info on header

--- src/main/java/com/poly/viettutor/controller/AuthController.java
+++ src/main/java/com/poly/viettutor/controller/AuthController.java
@@ -6,7 +6,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.poly.viettutor.dto.RegisterRequest;
 import com.poly.viettutor.service.UserService;
