69f87b7949ddd6421be8e36781e5cb0f6449947c
HieuNT-2001 <hieuth0109@gmail.com>
2025-06-09 20:47:39 +0700

    add user service

--- src/main/java/com/poly/viettutor/dto/RegisterRequest.java
+++ src/main/java/com/poly/viettutor/dto/RegisterRequest.java
@@ -9,8 +9,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class RegisterRequest {
 
-    private String email;
     private String fullname;
+    private String email;
     private String password;
     private String confirmPassword;
 
