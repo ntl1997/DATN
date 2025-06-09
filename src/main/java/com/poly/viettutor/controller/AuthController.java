64faca8abccc427ab1d1a4bc47285c243d5a5fb0
HieuNT-2001 <hieuth0109@gmail.com>
2025-06-09 21:46:07 +0700

    add register page

--- src/main/java/com/poly/viettutor/controller/AuthController.java
+++ src/main/java/com/poly/viettutor/controller/AuthController.java
@@ -1,5 +1,24 @@
 package com.poly.viettutor.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AuthController {
+
+    @GetMapping("/login")
+    public String showLogin(Model model) {
+        model.addAttribute("title", "Đăng nhập");
+        model.addAttribute("content", "client/auth/login");
+        return "client/layout/index";
+    }
+
+    @GetMapping("/register")
+    public String showRegister(Model model) {
+        model.addAttribute("title", "Đăng ký");
+        model.addAttribute("content", "client/auth/register");
+        return "client/layout/index";
+    }
 
 }
