4e4df2bf34ee8c8b097687fc6343cfc1bc2d8368
HieuNT-2001 <hieuth0109@gmail.com>
2025-06-11 18:31:36 +0700

    Update UserService.java

--- src/main/java/com/poly/viettutor/service/UserService.java
+++ src/main/java/com/poly/viettutor/service/UserService.java
@@ -2,7 +2,6 @@ package com.poly.viettutor.service;
 
 import java.util.Date;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
