a1bc97f26820079728a487c6f12e3d317af13e15
Hoang-Long-dev99 <pepemeohppro@gmail.com>
2025-06-19 18:56:41 +0700

    update resource handler for user uploads

--- src/main/java/com/poly/viettutor/config/WebConfig.java
+++ src/main/java/com/poly/viettutor/config/WebConfig.java
@@ -9,8 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
+        registry.addResourceHandler("/uploads/users/**")
+                .addResourceLocations("file:uploads/users/");
     }
 
 }
