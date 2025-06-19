ff8d8d7a7ef000a4e885336222f9aac75ef90263
ntl1997 <luandajk4@gmail.com>
2025-06-19 21:22:08 +0700

    Remove unused CourseCategoryRepository

--- src/main/java/com/poly/viettutor/repository/CourseCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.poly.viettutor.repository;
-
-import com.poly.viettutor.model.CourseCategory;
-import com.poly.viettutor.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseCategoryRepository extends JpaRepository<CourseCategory, CourseCategory.PK> {
-    long countByCategory(Category category);
-}
