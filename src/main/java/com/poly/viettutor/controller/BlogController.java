ed8170483fa54c90aa45cf5c4b878247aed910c6
Tunmbg <phamtungzgzg@gmail.com>
2025-06-19 12:29:09 +0700

    Thêm đường link blogDetails vào BlogController

--- src/main/java/com/poly/viettutor/controller/BlogController.java
+++ src/main/java/com/poly/viettutor/controller/BlogController.java
@@ -3,6 +3,7 @@ package com.poly.viettutor.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
@@ -10,6 +11,13 @@ public class BlogController {
     public String showBlog(Model model) {
         model.addAttribute("content", "client/layout/blog-list");
         model.addAttribute("title", "Bài viết");
+        return "client/layout/index";
+    }
+
+    @GetMapping("/blog-details")
+    public String showBlogDetails(Model model, @RequestParam("title") String title) {
+        model.addAttribute("content", "client/layout/blog-details");
+        model.addAttribute("title", title);
         return "client/layout/index";
     }
 
