32ba058b1fc11fe8deb6eb2b199dc4a822b91370
ntl1997 <luandajk4@gmail.com>
2025-06-22 17:19:36 +0700

    Add totalDuration method to CourseService for calculating course duration

--- src/main/java/com/poly/viettutor/service/CourseService.java
+++ src/main/java/com/poly/viettutor/service/CourseService.java
@@ -1,6 +1,7 @@
 package com.poly.viettutor.service;
 
 import com.poly.viettutor.model.Course;
+import com.poly.viettutor.model.Lecture;
 import com.poly.viettutor.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
@@ -32,5 +33,12 @@ public class CourseService {
 
     public List<Course> getTop6PopularCourses() {
         return courseRepository.findTop6PopularCourses(PageRequest.of(0, 6));
+    }
+
+    public int totalDuration(Course course) {
+        return course.getModules().stream()
+                .flatMap(module -> module.getLectures().stream())
+                .mapToInt(Lecture::getDuration)
+                .sum();
     }
 }
