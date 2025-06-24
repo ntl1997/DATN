90bf6a4d5549a3afcd88975f2617125a657fcad4
Tunmbg <phamtungzgzg@gmail.com>
2025-06-24 11:07:50 +0700

    Gắn link baiViet vô navbar mới

--- src/main/java/com/poly/viettutor/controller/BlogController.java
+++ src/main/java/com/poly/viettutor/controller/BlogController.java
@@ -1,24 +1,37 @@
 package com.poly.viettutor.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.poly.viettutor.model.BlogPost;
+import com.poly.viettutor.service.BlogPostService;
+
 @Controller
 public class BlogController {
-    @GetMapping("/blog-list")
+
+    @Autowired
+    private BlogPostService bService;
+
+    @GetMapping("/bai-viet")
     public String showBlog(Model model) {
-        model.addAttribute("content", "client/layout/blog-list");
+        List<BlogPost> bPosts = bService.findAll();
+        model.addAttribute("blogPosts", bPosts);
+        model.addAttribute("content", "client/blog/blog-list");
         model.addAttribute("title", "Bài viết");
         return "client/layout/index";
     }
 
-    @GetMapping("/blog-details")
-    public String showBlogDetails(Model model, @RequestParam("title") String title) {
-        model.addAttribute("content", "client/layout/blog-details");
-        model.addAttribute("title", title);
-        return "client/layout/index";
-    }
+    // @GetMapping("/blog-details")
+    // public String showBlogDetails(Model model, @RequestParam("title") String
+    // title) {
+    // model.addAttribute("content", "client/blog/blog-details");
+    // model.addAttribute("title", title);
+    // return "client/layout/index";
+    // }
 
 }
