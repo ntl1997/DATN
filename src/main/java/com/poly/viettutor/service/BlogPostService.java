9e93caf8ddbc9ddf8140534d85c3d3c03851c6a2
Tunmbg <phamtungzgzg@gmail.com>
2025-06-28 05:11:13 +0700

    Thêm Page trong BlogPostService

--- src/main/java/com/poly/viettutor/service/BlogPostService.java
+++ src/main/java/com/poly/viettutor/service/BlogPostService.java
@@ -2,6 +2,9 @@ package com.poly.viettutor.service;
 
 import com.poly.viettutor.model.BlogPost;
 import com.poly.viettutor.repository.BlogPostRepository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,5 +33,9 @@ public class BlogPostService {
 
     public void deleteById(Integer id) {
         blogPostRepository.deleteById(id);
+    }
+
+    public Page<BlogPost> findAll(Pageable pageable) {
+        return blogPostRepository.findAll(pageable);
     }
 }
