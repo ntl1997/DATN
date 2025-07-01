df80de1c665f6f5e8aaa5e0b897a56ff90828474
Nguyễn Kim Dũng <nguyenkimdung1501@gmail.com>
2025-07-01 14:35:42 +0700

    Add CertificateRepository

---
+++ src/main/java/com/poly/viettutor/service/CertificateService.java
@@ -0,0 +1,13 @@
+package com.poly.viettutor.service;
+
+import com.poly.viettutor.model.Certificate;
+import java.util.List;
+
+public interface CertificateService {
+
+    // CertificateService.java
+    List<Certificate> getCertificatesByUserId(Long userId);
+    List<Certificate> searchCertificatesByUserAndTitle(Long userId, String keyword);
+
+    Certificate getCertificateById(Integer certificateId);
+}
