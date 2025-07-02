2b569bedccee23e9aa905fb9d4d82e8e11bef1fc
Hoang-Long-dev99 <pepemeohppro@gmail.com>
2025-07-02 16:12:18 +0700

    Add ContactService class to handle contact information operations

---
+++ src/main/java/com/poly/viettutor/service/ContactService.java
@@ -0,0 +1,17 @@
+package com.poly.viettutor.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.poly.viettutor.model.ContactInfo;
+import com.poly.viettutor.repository.ContactRepository;
+
+@Service
+public class ContactService {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    public ContactInfo save(ContactInfo contactInfo) {
+        return contactRepository.save(contactInfo);
+    }
+}
