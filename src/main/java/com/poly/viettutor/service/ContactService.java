8f8fb6fa326632651a339d1111be4e3affa26b21
Hoang-Long-dev99 <pepemeohppro@gmail.com>
2025-07-03 15:57:16 +0700

    Add findAll method to ContactService for retrieving all contact information

--- src/main/java/com/poly/viettutor/service/ContactService.java
+++ src/main/java/com/poly/viettutor/service/ContactService.java
@@ -1,5 +1,7 @@
 package com.poly.viettutor.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.poly.viettutor.model.ContactInfo;
@@ -10,6 +12,10 @@ public class ContactService {
 
     @Autowired
     private ContactRepository contactRepository;
+
+    public List<ContactInfo> findAll() {
+        return contactRepository.findAll();
+    }
 
     public ContactInfo save(ContactInfo contactInfo) {
         return contactRepository.save(contactInfo);
