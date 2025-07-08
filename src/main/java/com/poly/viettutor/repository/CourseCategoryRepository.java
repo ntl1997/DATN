263adede0a237a9e226e3ad15abf7ecdde5c27aa
HieuNT-2001 <hieuth0109@gmail.com>
2025-07-09 04:46:10 +0700

    Update CourseCategoryRepository.java

--- src/main/java/com/poly/viettutor/repository/CourseCategoryRepository.java
+++ src/main/java/com/poly/viettutor/repository/CourseCategoryRepository.java
@@ -1,9 +1,11 @@
 package com.poly.viettutor.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.poly.viettutor.model.CourseCategory;
 
+@Repository
 public interface CourseCategoryRepository extends JpaRepository<CourseCategory, CourseCategory.PK> {
 
 }
