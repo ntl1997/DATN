80e645f4861092bbe2fde3ed50b7d1fc720e4c5e
HieuNT-2001 <hieuth0109@gmail.com>
2025-07-09 08:15:52 +0700

    Update ModuleDTO.java

--- src/main/java/com/poly/viettutor/dto/ModuleDTO.java
+++ src/main/java/com/poly/viettutor/dto/ModuleDTO.java
@@ -2,6 +2,7 @@ package com.poly.viettutor.dto;
 
 import java.util.List;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -17,6 +18,6 @@ public class ModuleDTO {
     private String moduleTitle;
 
     @NotEmpty(message = "Mỗi chương phải có ít nhất 1 bài học!")
-    private List<LectureDTO> lectures;
+    private List<@Valid LectureDTO> lectures;
 
 }
