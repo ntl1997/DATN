e734aa3371d885de9dfd218fef53c7cbe8bd8349
Hoang-Long-dev99 <pepemeohppro@gmail.com>
2025-07-12 20:08:31 +0700

    Add method to find wishlist by user and course

--- src/main/java/com/poly/viettutor/repository/WishListRepository.java
+++ src/main/java/com/poly/viettutor/repository/WishListRepository.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.poly.viettutor.model.Course;
 import com.poly.viettutor.model.User;
 import com.poly.viettutor.model.Wishlist;
 
 @Repository
 public interface WishListRepository extends JpaRepository<Wishlist, Integer> {
     List<Wishlist> findByUser(User user);
+
+    Wishlist findByUserAndCourse(User user, Course course);
 }
