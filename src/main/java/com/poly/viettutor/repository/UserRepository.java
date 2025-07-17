49eb814791e24145c8ff0c5a4203302892327606
HieuNT-2001 <hieuth0109@gmail.com>
2025-07-17 21:26:21 +0700

    Update UserRepository.java

--- src/main/java/com/poly/viettutor/repository/UserRepository.java
+++ src/main/java/com/poly/viettutor/repository/UserRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.poly.viettutor.model.User;
@@ -18,5 +20,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     // Tìm kiếm người dùng theo Role
     // Ví dụ: tìm kiếm người dùng có role là "INSTRUCTOR"
     List<User> findByRolesRoleName(String roleName);
+
+    // Tìm kiếm người dùng theo Role (nhiều role)
+    @Query("SELECT DISTINCT u FROM User u JOIN u.roles r WHERE r.roleName IN :roleNames")
+    List<User> findByRolesIn(@Param("roleNames") List<String> roleNames);
 
 }
