0f38aebe182b643630bdc36c85cfd267b95e558c
HieuNT-2001 <hieuth0109@gmail.com>
2025-07-17 21:26:16 +0700

    Update RegisterRequest.java

--- src/main/java/com/poly/viettutor/dto/RegisterRequest.java
+++ src/main/java/com/poly/viettutor/dto/RegisterRequest.java
@@ -16,10 +16,18 @@ public class RegisterRequest {
     @NotEmpty(message = "Email không được để trống")
     private String email;
 
+    @NotEmpty(message = "Số điện thoại không được để trống")
+    private String phoneNumber;
+
+    @NotEmpty(message = "Chức vụ không được để trống")
+    private String occupation;
+
     @NotEmpty(message = "Mật khẩu không được để trống")
     private String password;
 
     @NotEmpty(message = "Xác nhận mật khẩu không được để trống")
     private String confirmPassword;
+
+    Boolean isInstructor = false; // Mặc định là false, có thể được cập nhật khi tạo tài khoản
 
 }
