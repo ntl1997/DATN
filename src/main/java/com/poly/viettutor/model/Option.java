592eab4745c1c21c8e35b85c385e2f6fd51adcc7
HieuNT-2001 <hieuth0109@gmail.com>
2025-07-18 20:23:09 +0700

    Create Option.java

---
+++ src/main/java/com/poly/viettutor/model/Option.java
@@ -0,0 +1,35 @@
+package com.poly.viettutor.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "Options")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Option {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer optionId;
+
+    @ManyToOne
+    @JoinColumn(name = "questionId")
+    private Question question;
+
+    private String optionText;
+
+    private Boolean isCorrect;
+
+}
