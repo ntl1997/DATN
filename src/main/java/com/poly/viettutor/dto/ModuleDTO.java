3d6049e09e1385bf67fcd1e859f4a6a4212d4666
HieuNT-2001 <hieuth0109@gmail.com>
2025-07-18 23:54:27 +0700

    Update ModuleDTO.java

--- src/main/java/com/poly/viettutor/dto/ModuleDTO.java
+++ src/main/java/com/poly/viettutor/dto/ModuleDTO.java
@@ -5,13 +5,17 @@ import java.util.List;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ModuleDTO {
+
+    private Integer moduleId;
 
     @NotBlank(message = "Tiêu đề chương không được để trống!")
     private String moduleTitle;
