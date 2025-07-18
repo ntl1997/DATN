bec5fc9560117bb5984c6a2e041e9cbc41205878
HieuNT-2001 <hieuth0109@gmail.com>
2025-07-18 23:54:46 +0700

    Update CourseModuleRepository.java

--- src/main/java/com/poly/viettutor/repository/CourseModuleRepository.java
+++ src/main/java/com/poly/viettutor/repository/CourseModuleRepository.java
@@ -1,11 +1,16 @@
 package com.poly.viettutor.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.poly.viettutor.model.Course;
 import com.poly.viettutor.model.CourseModule;
 
 @Repository
 public interface CourseModuleRepository extends JpaRepository<CourseModule, Integer> {
+
+    List<CourseModule> findByCourse(Course course);
 
 }
