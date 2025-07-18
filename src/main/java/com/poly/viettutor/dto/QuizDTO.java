e87a128df16c7769791a05b503a3888a81a33284
HieuNT-2001 <hieuth0109@gmail.com>
2025-07-18 23:54:32 +0700

    Update QuizDTO.java

--- src/main/java/com/poly/viettutor/dto/QuizDTO.java
+++ src/main/java/com/poly/viettutor/dto/QuizDTO.java
@@ -5,12 +5,14 @@ import java.util.List;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class QuizDTO {
 
     @NotBlank(message = "Tiêu đề quiz không được để trống!")
