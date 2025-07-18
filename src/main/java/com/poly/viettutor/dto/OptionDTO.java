4e4f564ffca0e9344a862bfe2ac1c25705dca27e
HieuNT-2001 <hieuth0109@gmail.com>
2025-07-18 15:16:58 +0700

    Create OptionDTO.java

---
+++ src/main/java/com/poly/viettutor/dto/OptionDTO.java
@@ -0,0 +1,19 @@
+package com.poly.viettutor.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OptionDTO {
+
+    @NotBlank(message = "Câu trả lời không được để trống!")
+    private String optionText;
+
+    @NotBlank(message = "Trạng thái không được để trống!")
+    private Boolean isCorrect = false;
+
+}
