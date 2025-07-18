2d2257bd055175da7cf74c23b54311163310eb1b
HieuNT-2001 <hieuth0109@gmail.com>
2025-07-18 20:23:11 +0700

    Create Question.java

---
+++ src/main/java/com/poly/viettutor/model/Question.java
@@ -0,0 +1,41 @@
+package com.poly.viettutor.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "Questions")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer questionId;
+
+    @ManyToOne
+    @JoinColumn(name = "quizId")
+    private Quiz quiz;
+
+    private String questionText;
+
+    private Integer score;
+
+    @OneToMany(mappedBy = "question")
+    private List<Option> options;
+
+}
