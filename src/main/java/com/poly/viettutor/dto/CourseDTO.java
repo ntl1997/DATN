cf4779f70115e32a29fb1d8f060296028400cd19
HieuNT-2001 <hieuth0109@gmail.com>
2025-07-18 23:54:24 +0700

    Update CourseDTO.java

--- src/main/java/com/poly/viettutor/dto/CourseDTO.java
+++ src/main/java/com/poly/viettutor/dto/CourseDTO.java
@@ -1,12 +1,19 @@
 package com.poly.viettutor.dto;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import com.poly.viettutor.model.Course;
+import com.poly.viettutor.model.CourseModule;
+import com.poly.viettutor.model.Lecture;
+import com.poly.viettutor.model.Option;
+import com.poly.viettutor.model.Question;
+import com.poly.viettutor.model.Quiz;
 
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,6 +25,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class CourseDTO {
+
+    private Integer courseId;
 
     @NotBlank(message = "Tiêu đề không được để trống!")
     private String title;
@@ -56,7 +65,67 @@ public class CourseDTO {
     @NotEmpty(message = "Chọn ít nhất một danh mục!")
     private List<Integer> categoryIds;
 
-    @NotEmpty(message = "Khóa học phải có ít nhất 1 chương!")
     private List<ModuleDTO> modules;
+
+    public CourseDTO toDTO(Course course) {
+        return CourseDTO.builder()
+                .courseId(course.getCourseId())
+                .title(course.getTitle())
+                .description(course.getDescription())
+                .overview(course.getOverview())
+                .price(course.getPrice())
+                .discount(course.getDiscount())
+                .courseImage(course.getCourseImage())
+                .demoVideoUrl(course.getDemoVideoUrl())
+                .status(course.getStatus())
+                .skillLevel(course.getSkillLevel())
+                .hasCertificate(course.getHasCertificate())
+                .language(course.getLanguage())
+                .categoryIds(course.getCourseCategories()
+                        .stream().map(cc -> cc.getCategory().getCategoryId()).collect(Collectors.toList()))
+                .modules(course.getModules().stream().map(this::mapModule).toList())
+                .build();
+    }
+
+    private ModuleDTO mapModule(CourseModule module) {
+        return ModuleDTO.builder()
+                .moduleTitle(module.getModuleTitle())
+                .lectures(module.getLectures().stream().map(this::mapLecture).toList())
+                .quizzes(module.getQuizzes().stream().map(this::mapQuiz).toList())
+                .build();
+    }
+
+    private LectureDTO mapLecture(Lecture lecture) {
+        return LectureDTO.builder()
+                .lectureTitle(lecture.getLectureTitle())
+                .content(lecture.getContent())
+                .videoUrl(lecture.getVideoUrl())
+                .duration(lecture.getDuration())
+                .build();
+    }
+
+    private QuizDTO mapQuiz(Quiz quiz) {
+        return QuizDTO.builder()
+                .title(quiz.getTitle())
+                .timeLimit(quiz.getTimeLimit())
+                .totalScore(quiz.getTotalScore().doubleValue())
+                .questions(quiz.getQuestions().stream().map(this::mapQuestion).toList())
+                .build();
+    }
+
+    private QuestionDTO mapQuestion(Question question) {
+        return QuestionDTO.builder()
+                .questionText(question.getQuestionText())
+                .score(question.getScore().doubleValue())
+                .options(question.getOptions().stream().map(this::mapOption).toList())
+                .build();
+    }
+
+    private OptionDTO mapOption(Option option) {
+        return OptionDTO.builder()
+                .optionText(option.getOptionText())
+                .isCorrect(option.getIsCorrect())
+                .build();
+    }
 
 }
