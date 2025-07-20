c2b4159ba4b4e1977ab6d75f500df6d185d8ca05
HieuNT-2001-Student <hieuntpp03096@fpt.edu.vn>
2025-07-20 17:01:41 +0700

    Update Question.java

--- src/main/java/com/poly/viettutor/model/Question.java
+++ src/main/java/com/poly/viettutor/model/Question.java
@@ -3,6 +3,7 @@ package com.poly.viettutor.model;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -32,6 +33,7 @@ public class Question {
     @JoinColumn(name = "quizId")
     private Quiz quiz;
 
+    @Column(columnDefinition = "NVARCHAR(MAX)")
     private String questionText;
 
     private Integer score;
