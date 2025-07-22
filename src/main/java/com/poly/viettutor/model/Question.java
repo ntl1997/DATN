8c589d08e214100103f76a8ce596a34d3bf7168a
ntl1997 <luandajk4@gmail.com>
2025-07-22 22:10:57 +0700

    Create

---
+++ src/main/java/com/poly/viettutor/model/Question.java
@@ -0,0 +1,31 @@
+package com.poly.viettutor.model;
+
+import lombok.*;
+
+import java.util.List;
+import java.util.Set;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Questions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long questionId;
+
+    @ManyToOne
+    @JoinColumn(name = "quizId")
+    private Quiz quiz;
+
+    @Column(columnDefinition = "NVARCHAR(MAX)")
+    private String questionText;
+
+    private Integer score;
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Option> options;
+}
