4c3cc265d6bbbeb8639f4a2d9638b65a0e815333
HieuNT-2001 <hieuth0109@gmail.com>
2025-07-23 17:31:28 +0700

    fix display bar chart

--- src/main/resources/static/admin-assets/js/pages/ui-chartjs.js
+++ src/main/resources/static/admin-assets/js/pages/ui-chartjs.js
@@ -288,12 +288,12 @@ var ctxBar = document.getElementById("bar").getContext("2d");
 var myBar = new Chart(ctxBar, {
   type: "bar",
   data: {
-    labels: teacherLabels,
+    labels: labels,
     datasets: [
       {
         label: "Số lượng học sinh",
         backgroundColor: [chartColors.blue],
-        data: studentData,
+        data: data,
       },
     ],
   },
@@ -325,142 +325,101 @@ var myBar = new Chart(ctxBar, {
   },
 });
 
-var ctxBar2 = document.getElementById("bar2").getContext("2d");
-var myBar2 = new Chart(ctxBar2, {
-  type: "bar",
-  data: {
-    labels: courseLabels,
-    datasets: [
-      {
-        label: "Số lượng học sinh",
-        backgroundColor: [chartColors.blue],
-        data: courseData,
-      },
-    ],
-  },
-  options: {
-    responsive: true,
-    plugins: {
-      legend: {
-        display: true,
-      },
-    },
-    scales: {
-      y: {
-        beginAtZero: true,
-        suggestedMax: 1000,
-        ticks: {
-          padding: 10,
-        },
-        grid: {
-          drawBorder: false,
-        },
-      },
-      x: {
-        grid: {
-          display: false,
-          drawBorder: false,
-        },
-      },
-    },
-  },
-});
+// var line = document.getElementById("line").getContext("2d");
+// var gradient = line.createLinearGradient(0, 0, 0, 400);
+// gradient.addColorStop(0, "rgba(50, 69, 209,1)");
+// gradient.addColorStop(1, "rgba(265, 177, 249,0)");
 
-var line = document.getElementById("line").getContext("2d");
-var gradient = line.createLinearGradient(0, 0, 0, 400);
-gradient.addColorStop(0, "rgba(50, 69, 209,1)");
-gradient.addColorStop(1, "rgba(265, 177, 249,0)");
+// var gradient2 = line.createLinearGradient(0, 0, 0, 400);
+// gradient2.addColorStop(0, "rgba(255, 91, 92,1)");
+// gradient2.addColorStop(1, "rgba(265, 177, 249,0)");
 
-var gradient2 = line.createLinearGradient(0, 0, 0, 400);
-gradient2.addColorStop(0, "rgba(255, 91, 92,1)");
-gradient2.addColorStop(1, "rgba(265, 177, 249,0)");
-
-var myline = new Chart(line, {
-  type: "line",
-  data: {
-    labels: [
-      "16-07-2018",
-      "17-07-2018",
-      "18-07-2018",
-      "19-07-2018",
-      "20-07-2018",
-      "21-07-2018",
-      "22-07-2018",
-      "23-07-2018",
-      "24-07-2018",
-      "25-07-2018",
-    ],
-    datasets: [
-      {
-        label: "Balance",
-        data: [50, 25, 61, 50, 72, 52, 60, 41, 30, 45],
-        backgroundColor: "rgba(50, 69, 209,.6)",
-        borderWidth: 3,
-        borderColor: "rgba(63,82,227,1)",
-        pointBorderWidth: 0,
-        pointBorderColor: "transparent",
-        pointRadius: 3,
-        pointBackgroundColor: "transparent",
-        pointHoverBackgroundColor: "rgba(63,82,227,1)",
-      },
-      {
-        label: "Balance",
-        data: [20, 35, 45, 75, 37, 86, 45, 65, 25, 53],
-        backgroundColor: "rgba(253, 183, 90,.6)",
-        borderWidth: 3,
-        borderColor: "rgba(253, 183, 90,.6)",
-        pointBorderWidth: 0,
-        pointBorderColor: "transparent",
-        pointRadius: 3,
-        pointBackgroundColor: "transparent",
-        pointHoverBackgroundColor: "rgba(63,82,227,1)",
-      },
-    ],
-  },
-  options: {
-    responsive: true,
-    layout: {
-      padding: {
-        top: 10,
-      },
-    },
-    tooltips: {
-      intersect: false,
-      titleFontFamily: "Helvetica",
-      titleMarginBottom: 10,
-      xPadding: 10,
-      yPadding: 10,
-      cornerRadius: 3,
-    },
-    legend: {
-      display: true,
-    },
-    scales: {
-      yAxes: [
-        {
-          gridLines: {
-            display: true,
-            drawBorder: true,
-          },
-          ticks: {
-            display: true,
-          },
-        },
-      ],
-      xAxes: [
-        {
-          gridLines: {
-            drawBorder: false,
-            display: false,
-          },
-          ticks: {
-            display: false,
-          },
-        },
-      ],
-    },
-  },
-});
+// var myline = new Chart(line, {
+//   type: "line",
+//   data: {
+//     labels: [
+//       "16-07-2018",
+//       "17-07-2018",
+//       "18-07-2018",
+//       "19-07-2018",
+//       "20-07-2018",
+//       "21-07-2018",
+//       "22-07-2018",
+//       "23-07-2018",
+//       "24-07-2018",
+//       "25-07-2018",
+//     ],
+//     datasets: [
+//       {
+//         label: "Balance",
+//         data: [50, 25, 61, 50, 72, 52, 60, 41, 30, 45],
+//         backgroundColor: "rgba(50, 69, 209,.6)",
+//         borderWidth: 3,
+//         borderColor: "rgba(63,82,227,1)",
+//         pointBorderWidth: 0,
+//         pointBorderColor: "transparent",
+//         pointRadius: 3,
+//         pointBackgroundColor: "transparent",
+//         pointHoverBackgroundColor: "rgba(63,82,227,1)",
+//       },
+//       {
+//         label: "Balance",
+//         data: [20, 35, 45, 75, 37, 86, 45, 65, 25, 53],
+//         backgroundColor: "rgba(253, 183, 90,.6)",
+//         borderWidth: 3,
+//         borderColor: "rgba(253, 183, 90,.6)",
+//         pointBorderWidth: 0,
+//         pointBorderColor: "transparent",
+//         pointRadius: 3,
+//         pointBackgroundColor: "transparent",
+//         pointHoverBackgroundColor: "rgba(63,82,227,1)",
+//       },
+//     ],
+//   },
+//   options: {
+//     responsive: true,
+//     layout: {
+//       padding: {
+//         top: 10,
+//       },
+//     },
+//     tooltips: {
+//       intersect: false,
+//       titleFontFamily: "Helvetica",
+//       titleMarginBottom: 10,
+//       xPadding: 10,
+//       yPadding: 10,
+//       cornerRadius: 3,
+//     },
+//     legend: {
+//       display: true,
+//     },
+//     scales: {
+//       yAxes: [
+//         {
+//           gridLines: {
+//             display: true,
+//             drawBorder: true,
+//           },
+//           ticks: {
+//             display: true,
+//           },
+//         },
+//       ],
+//       xAxes: [
+//         {
+//           gridLines: {
+//             drawBorder: false,
+//             display: false,
+//           },
+//           ticks: {
+//             display: false,
+//           },
+//         },
+//       ],
+//     },
+//   },
+// });
 
 // let ctx1 = document.getElementById("canvas1").getContext("2d");
 // let ctx2 = document.getElementById("canvas2").getContext("2d");
