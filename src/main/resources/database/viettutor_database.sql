966437825591a3112c45036632f4cf1666baaaca
HieuNT-2001 <hieuth0109@gmail.com>
2025-07-23 19:04:34 +0700

    Update viettutor_database.sql

--- src/main/resources/database/viettutor_database.sql
+++ src/main/resources/database/viettutor_database.sql
@@ -52,7 +52,8 @@ CREATE TABLE Users
     CreatedAt DATETIME DEFAULT GETDATE(),
     PhoneNumber NVARCHAR(20),
     Occupation NVARCHAR(100),
-    Biography NVARCHAR(MAX)
+    Biography NVARCHAR(MAX),
+    Status BIT DEFAULT 1,
 );
 GO
 
