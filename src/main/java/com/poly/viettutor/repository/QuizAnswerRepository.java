85d99663b2e40b6410422dacbb2e540c418c475f
ntl1997 <luandajk4@gmail.com>
2025-07-24 21:41:36 +0700

    Add

--- src/main/java/com/poly/viettutor/repository/QuizAnswerRepository.java
+++ src/main/java/com/poly/viettutor/repository/QuizAnswerRepository.java
@@ -1,10 +1,11 @@
 package com.poly.viettutor.repository;
 
 import com.poly.viettutor.model.QuizAnswer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuizAnswerRepository extends JpaRepository<QuizAnswer, Long> {
