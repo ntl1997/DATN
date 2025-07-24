7d71c380d81260dcbad5d7d193b1e8913bc22170
ntl1997 <luandajk4@gmail.com>
2025-07-24 21:41:47 +0700

    Thêm Repo

--- src/main/java/com/poly/viettutor/repository/QuizSubmissionRepository.java
+++ src/main/java/com/poly/viettutor/repository/QuizSubmissionRepository.java
@@ -1,14 +1,14 @@
 package com.poly.viettutor.repository;
 
 import com.poly.viettutor.model.QuizSubmission;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 @Repository
+
 public interface QuizSubmissionRepository extends JpaRepository<QuizSubmission, Long> {
     List<QuizSubmission> findByQuizQuizId(Long quizId);
-
-    List<QuizSubmission> findByUserId(Long userId); // Change 'userId' to 'user.id'
 }
