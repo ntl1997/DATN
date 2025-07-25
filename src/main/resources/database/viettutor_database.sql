7cff9a17a3bd74f71a064f6931afc767e0994d7a
Tunmbg <phamtungzgzg@gmail.com>
2025-07-25 15:50:43 +0700

    Update

--- src/main/resources/database/viettutor_database.sql
+++ src/main/resources/database/viettutor_database.sql
@@ -630,7 +630,8 @@ GO
 INSERT INTO Quizzes
     (ModuleId, Title, TotalScore, TimeLimit)
 VALUES
-    (1, N'Quiz Giới thiệu Python', 10, 15);
+    (1, N'Quiz Giới thiệu Python', 10, 15),
+    (3, N'Quiz HTML cơ bản', 10, 15);
 
 -- 24. Thêm 2 câu hỏi trắc nghiệm
 INSERT INTO Questions
